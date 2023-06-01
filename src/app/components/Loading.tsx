16ea89cf7995477a6fdd694c48f3f91707bf277b
Noah Lu <15753140326@163.com>
2023-06-01 23:49:05 +0800

    feat: initial

---
+++ src/app/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen opacity-50">
+      <div className="w-10 h-10 border-4 border-t-4 rounded-full border-t-gray-600 animate-spin"></div>
+    </div>
+  );
+}
